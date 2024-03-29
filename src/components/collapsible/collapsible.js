a77b62c33dc492765cce7fa583cb7c245c2691ca
Egor Ivanov <rust_lang_lover@proton.me>
2024-03-29 13:09:43 +0300

    // Added two fields for notes

--- src/components/collapsible/collapsible.js
+++ src/components/collapsible/collapsible.js
@@ -2,9 +2,18 @@ import React from "react";
 import './collapsible.sass';
 
 const Collapsable_list = () => {
+
+    function create_text_element() { // -> Node // created element
+        let e = document.createElement('p');
+        e.textContent = "My-test";
+        e.className = "styled-content";
+        return e
+    }
+
     const unwrap_the_list = (event) => {
         const this_sample = event.target;
-        console.log(this_sample.parentNode);
+        console.log(this_sample.parentNode.lastChild);
+        this_sample.parentNode.lastChild.appendChild(create_text_element());
     };
 
     return (
@@ -12,13 +21,13 @@ const Collapsable_list = () => {
             <div className="collapsible-container">
                 <button className="collapsible-container__activator" onClick={unwrap_the_list}>Пометки</button>
                 <div className="collapsible-container__content">
-                    <p>Lorem ipsum...</p>
+
                 </div>
             </div>
             <div className="collapsible-container">
                 <button className="collapsible-container__activator" onClick={unwrap_the_list}>Логи</button>
                 <div className="collapsible-container__content">
-                    <p>Lorem ipsum...</p>
+
                 </div>
             </div>
         </div>
