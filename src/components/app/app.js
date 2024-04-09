8c534a202ed8f725c3d23b9eea02b7757b49560a
Egor Ivanov <rust_lang_lover@proton.me>
2024-04-09 11:55:34 +0300

    // -> deploy

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,7 +7,6 @@ import {Alert} from "@mui/material";
 
 const App = () => {
     const [OrdersVector, setOrdersVector] = useState([]);
-    const [showAlert, setShowAlert] = useState(false);
     useEffect(() => {
         fetch("http://localhost:8000/api/orders/get", {
             method : "POST",
@@ -33,7 +32,6 @@ const App = () => {
     return (
         <div className='app_flex_centered_vertical'>
             <BlackHeader/>
-            <Alert sx={{position : "absolute", left : "2.5%", bottom : "5%", zIndex : "3"}} variant="filled" severity="success">This is a success Alert.</Alert>
             <BasicTable object_vector={OrdersVector} reload_orders={setOrdersVector}/>
         </div>
     )
