aad95825713e5074f76302c38f2e157ce0afafaf
Egor Ivanov <rust_lang_lover@proton.me>
2024-04-09 15:38:48 +0300

    // Alert ???????

--- src/components/auth/auth.component.jsx
+++ src/components/auth/auth.component.jsx
@@ -1,11 +1,36 @@
-import {useState} from "react";
-import {Button, TextField} from "@mui/material";
+import {useEffect, useState} from "react";
+import {Alert, Button, TextField} from "@mui/material";
 import "./auth.style.sass";
 import LoginIcon from '@mui/icons-material/Login';
 
 const Auth = (props) => {
     const [login, setLogin] = useState("");
     const [password, setPassword] = useState("");
+    const delay = ms => new Promise(res => setTimeout(res, ms))
+    const [showAlert, setShowAlert] = useState([]);
+
+    useEffect(() => {
+        async function wait() {
+            await delay(3000);
+            setShowAlert([]);
+        }
+    }, [showAlert.length])
+
+    function display_alert() {
+        if (showAlert.length != 0) {
+            return (
+                <Alert
+                    sx={{position : "fixed", top : "30%", width : "38%"}}
+                    variant="filled"
+                    severity={showAlert.severity}
+                >
+                    {showAlert.text}
+                </Alert>
+            )
+        }
+    }
+
+
 
     function handle_login_attempt() {
         fetch("http://localhost:8000/api/login/attempt", {
@@ -24,13 +49,25 @@ const Auth = (props) => {
                     props.setIsAuthorized(true)
                 }
                 else {
-                    console.log(json.message)
+                    setShowAlert([{
+                        condition : true,
+                        text : `${json.message}`,
+                        severity : "error"
+                    }])
                 }
+            })
+            .catch(function(err) {
+                setShowAlert([{
+                    condition : true,
+                    text : `${err}`,
+                    severity : "error"
+                }])
             })
     }
 
     return (
         <div className='auth-container'>
+            {display_alert()}
             <TextField
                 sx={{width : "40%"}}
                 label="Логин"
