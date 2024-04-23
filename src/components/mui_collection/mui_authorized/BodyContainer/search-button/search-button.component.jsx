5ed971ee043f58f1716f487657e65e33e2a52a7e
Egor Ivanov <rust_lang_lover@proton.me>
2024-04-23 18:18:49 +0300

    // if filter is active - search button is changing

--- src/components/mui_collection/mui_authorized/BodyContainer/search-button/search-button.component.jsx
+++ src/components/mui_collection/mui_authorized/BodyContainer/search-button/search-button.component.jsx
@@ -1,26 +1,49 @@
 import SearchIcon from '@mui/icons-material/Search';
 import {Button} from "@mui/material";
+import RotateLeftIcon from "@mui/icons-material/RotateLeft";
 
 const SearchButton = (props) => {
+
+    const placement = {
+        gridColumn : "35 / 40",
+        gridRow : "2 / 2"
+    };
 
     function request_search_by_query() {
         props.setFilterCondition(!props.filterCondition);
         console.log(props.filterCondition);
     }
 
-    return (
-        <Button
-            sx={{
-                gridColumn : "35 / 40",
-                gridRow : "2 / 2"
-            }}
-            variant="contained"
-            endIcon={<SearchIcon/>}
-            onClick={request_search_by_query}
-        >
-            Поиск
-        </Button>
-    )
+    function reset_the_search() {
+        props.setFilterCondition(!props.filterCondition);
+        console.log(props.filterCondition);
+    }
+
+    if (props.filterCondition) {
+        return (
+            <Button
+                sx={placement}
+                variant="contained"
+                color="error"
+                endIcon={<RotateLeftIcon/>}
+                onClick={reset_the_search}
+            >
+                Сброс
+            </Button>
+        )
+    }
+    else {
+        return (
+            <Button
+                sx={placement}
+                variant="contained"
+                endIcon={<SearchIcon/>}
+                onClick={request_search_by_query}
+            >
+                Поиск
+            </Button>
+        )
+    }
 }
 
 export default SearchButton;
