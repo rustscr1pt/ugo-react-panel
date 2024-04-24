6910dc460d2f434d9a94c0bd55823a0f7dd1d979
rust_lang_lover <rust_lang_lover@proton.me>
2024-04-24 14:08:46 +0300

    // Debug for date worker

--- src/components/mui_collection/mui_authorized/BodyContainer/DatePopper/date-picker/date-picker.component.jsx
+++ src/components/mui_collection/mui_authorized/BodyContainer/DatePopper/date-picker/date-picker.component.jsx
@@ -2,11 +2,19 @@ import * as React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import dayjs from "dayjs";
 
 export default function BasicDateCalendar() {
+    const [value, setValue] = React.useState(dayjs());
+
+    function manage_data_change(newValue) {
+        setValue(newValue)
+        console.log(`Day : ${newValue.$D} Month : ${newValue.$M + 1} Year : ${newValue.$y}`)
+    }
+
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar/>
+            <DateCalendar value={value} onChange={(newValue) => manage_data_change(newValue)}/>
         </LocalizationProvider>
     );
 }
