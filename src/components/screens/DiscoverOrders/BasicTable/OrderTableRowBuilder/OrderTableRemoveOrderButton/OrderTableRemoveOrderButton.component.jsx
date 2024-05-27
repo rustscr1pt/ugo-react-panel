d1094100165800e2c5b45fe7272432e29b36ae7a
rust_lang_lover <rust_lang_lover@proton.me>
2024-05-27 15:11:54 +0300

    // orders are now removed correctly

--- src/components/screens/DiscoverOrders/BasicTable/OrderTableRowBuilder/OrderTableRemoveOrderButton/OrderTableRemoveOrderButton.component.jsx
+++ src/components/screens/DiscoverOrders/BasicTable/OrderTableRowBuilder/OrderTableRemoveOrderButton/OrderTableRemoveOrderButton.component.jsx
@@ -2,18 +2,19 @@ import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import TableCell from "@mui/material/TableCell";
 import * as React from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RemoveOrderAndReload} from "./_functions/RemoveOrderAndReload";
 
 export const OrderTableRemoveOrderButton = (props) => {
     const dispatch = useDispatch();
+    const sourceType = useSelector((state) => state.selectedBase.value);
 
     return (
         <TableCell align="center">
             <IconButton
                 aria-label="delete"
                 size="small"
-                onClick={() => RemoveOrderAndReload(props.id, dispatch)}
+                onClick={() => RemoveOrderAndReload(props.id, dispatch, sourceType)}
             >
                 <DeleteIcon />
             </IconButton>
