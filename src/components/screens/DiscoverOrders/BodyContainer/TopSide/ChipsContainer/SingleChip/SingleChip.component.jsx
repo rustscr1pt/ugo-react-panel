9bc2fc0d0e2d9cf2fa2558fe47df852df89b10fe
rust_lang_lover <rust_lang_lover@proton.me>
2024-07-02 12:10:52 +0300

    // set adaptive font for SingleChip.component.jsx

--- src/components/screens/DiscoverOrders/BodyContainer/TopSide/ChipsContainer/SingleChip/SingleChip.component.jsx
+++ src/components/screens/DiscoverOrders/BodyContainer/TopSide/ChipsContainer/SingleChip/SingleChip.component.jsx
@@ -5,6 +5,7 @@ import {useSelector} from "react-redux";
 const pref_width = "49%";
 const blue_back = "#1976d2";
 const white_font = "#FFFFFF";
+const fontSize = "1vw";
 
 export const SingleChip = () => {
     const filterCondition = useSelector((state) => state.ordersFilterCondition.value);
@@ -12,16 +13,26 @@ export const SingleChip = () => {
     if (filterCondition) {
         return (
             <div className="chips-container-flex">
-                <Chip sx={{width : pref_width}} label="Все заказы" variant="outlined"/>
-                <Chip sx={{width : pref_width, color : white_font, backgroundColor : blue_back}} label="Фильтрация"/>
+                <Chip sx={{
+                    width : pref_width,
+                    fontSize : fontSize
+                }} label="Все заказы" variant="outlined"/>
+                <Chip sx={{
+                    width : pref_width, fontSize : fontSize, color : white_font, backgroundColor : blue_back
+                }} label="Фильтрация"/>
             </div>
         )
     }
     else {
         return (
             <div className="chips-container-flex">
-                <Chip sx={{width : pref_width, color : white_font, backgroundColor : blue_back}} label="Все заказы"/>
-                <Chip sx={{width : pref_width}} label="Фильтрация" variant="outlined"/>
+                <Chip sx={{
+                    width : pref_width, fontSize : fontSize, color : white_font, backgroundColor : blue_back
+                }} label="Все заказы"/>
+                <Chip sx={{
+                    width : pref_width,
+                    fontSize : fontSize
+                }} label="Фильтрация" variant="outlined"/>
             </div>
         )
     }
